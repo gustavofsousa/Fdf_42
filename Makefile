8e0c80284da7b173febbcdea1603294e351d82e4
gusousa <gfsousa42@gmail.com>
2022-08-23 17:05:51 -0300

    Using malloc to img

--- Makefile
+++ Makefile
@@ -9,14 +9,14 @@ SRCS = main_backblue.c
 OBJS = $(SRCS:.c=.o)
 
 %.o: %.c
-	$(CC) -Wall -Wextra -Werror -Imlx -c $< -o $@
+	$(CC) -g $(CFLAGS) -Imlx -c $< -o $@
 
 
 all: $(NAME)
 
 $(NAME): $(OBJS)
 	make -s -C mlx
-	$(CC) $(OBJS) -Lmlx -lmlx -framework OpenGL -framework AppKit -o $(NAME)
+	$(CC) $(OBJS) -g -Lmlx -lmlx -framework OpenGL -framework AppKit -o $(NAME)
 
 
 
--- main_backblue.c
+++ main_backblue.c
@@ -1,4 +1,5 @@
 #include "./mlx/mlx.h"
+#include <stdlib.h>
 
 typedef struct s_data
 {
@@ -20,30 +21,29 @@ int	main(void)
 
 	t_data data;
 	data.bits_per_pixel = 32;
-	line_bytes = 100;
+	line_bytes = 128;
+	data.endian = 1;
 
 	data.mlx = mlx_init();
 	data.win = mlx_new_window(data.mlx, 640, 360, "red window");
 	data.img = mlx_new_image(data.mlx, 640, 360);
+	data.img = malloc(640 * 4 * 360 * 4);
 
 	buffer = mlx_get_data_addr(data.img, &data.bits_per_pixel, &line_bytes, &data.endian);
 
 
 	color = 0xFF0000;
-	y = -1;
-	while (++y < 360)//Lines
+	y = 0;
+	while (y < 360)//Lines
 	{
-		x = -1;
-		while (++x < 640)//columns
+		x = 0;
+		while (x < 640)//columns
 		{
-			buffer[(y * line_bytes) + x] = color;
+			buffer[y + x] = color;
 
-
-
-
-
+			x += data.bits_per_pixel / 8;
 		}
-
+		y += line_bytes / 32;
 	}
 	mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
 	mlx_loop(data.mlx);
