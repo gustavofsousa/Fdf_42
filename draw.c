26629055d89aab1407c1668614202a621dd030aa
gusousa <gfsousa42@gmail.com>
2022-09-07 15:12:42 -0300

    norm mlx and draw

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@
 /*   By: gusousa <gusousa@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 14:45:32 by gusousa           #+#    #+#             */
-/*   Updated: 2022/09/07 14:47:54 by gusousa          ###   ########.fr       */
+/*   Updated: 2022/09/07 15:10:15 by gusousa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,35 @@
 
 void    draw_win(t_fdf *fdf)
 {
-        int                     pos;
-        int                     i;
-        int             j;
-        int stack_interval_row = W_HEIGHT / fdf->map.rows;
-        int stack_interval_col = W_LENGHT / fdf->map.columns;
+	int                     pos;
+	int                     i;
+	int             j;
+	int stack_interval_row = W_HEIGHT / fdf->map.rows;
+	int stack_interval_col = W_LENGHT / fdf->map.columns;
 
-        t_pixel pixel;
-        pixel.color = GREEN_1;
-        pixel.y = -1;
-        i = 0;
-        while (++pixel.y < W_HEIGHT - 50)
-        {
-                j = 0;
-                pixel.x = -1;
-                while (++pixel.x < W_LENGHT - 50)
-                {
-                        if (pixel.y  % stack_interval_row == 0 || pixel.x % stack_interval_col == 0)
-                        {
-                                pos = (pixel.y * fdf->mlx.line_bytes) + pixel.x;
-                                if (fdf->map.map[i][j] != 0)
-                                        fdf->mlx.buffer[pos] = PINK;
-                                else
-                                        fdf->mlx.buffer[pos] = pixel.color;
-                                if (pixel.x % stack_interval_col == 0)
-                                        j++;
-                        }
-                }
-                if (pixel.y % stack_interval_row == 0)
-                        i++;
-        }
-        mlx_put_image_to_window(fdf->mlx.mlx, fdf->mlx.win, fdf->mlx.img, 30, 30);
+	t_pixel pixel;
+	pixel.color = GREEN_1;
+	pixel.y = -1;
+	i = 0;
+	while (++pixel.y < W_HEIGHT - 50)
+	{
+		j = 0;
+		pixel.x = -1;
+		while (++pixel.x < W_LENGHT - 50)
+		{
+			if (pixel.y  % stack_interval_row == 0 || pixel.x % stack_interval_col == 0)
+			{
+				pos = (pixel.y * fdf->mlx.line_bytes) + pixel.x;
+				if (fdf->map.map[i][j] != 0)
+					fdf->mlx.buffer[pos] = PINK;
+				else
+					fdf->mlx.buffer[pos] = pixel.color;
+				if (pixel.x % stack_interval_col == 0)
+					j++;
+			}
+		}
+		if (pixel.y % stack_interval_row == 0)
+			i++;
+	}
+	mlx_put_image_to_window(fdf->mlx.mlx, fdf->mlx.win, fdf->mlx.img, 30, 30);
 }
--- mlx.c
+++ mlx.c
@@ -6,37 +6,37 @@
 /*   By: gusousa <gusousa@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 14:39:55 by gusousa           #+#    #+#             */
-/*   Updated: 2022/09/07 14:53:44 by gusousa          ###   ########.fr       */
+/*   Updated: 2022/09/07 15:11:48 by gusousa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static void add_mlx_hook(t_fdf *fdf)
+static void	add_mlx_hook(t_fdf *fdf)
 {
 	(void)fdf;	
 }
 
-int     display_mlx_win(t_fdf *fdf)
+int	display_mlx_win(t_fdf *fdf)
 {
-        fdf->mlx.mlx = mlx_init();
-        if (fdf->mlx.mlx != NULL)
-        {
-                fdf->mlx.win = mlx_new_window(fdf->mlx.mlx, W_LENGHT, W_HEIGHT, "fdf");
-                if (fdf->mlx.win != NULL)
-                {
-					fdf->mlx.img = mlx_new_image(fdf->mlx.mlx, W_LENGHT - 50, W_HEIGHT - 50);
-                        if (fdf->mlx.img != NULL)
-                        {
-                                fdf->mlx.buffer = (int *)mlx_get_data_addr(fdf->mlx.img, 
-										&fdf->mlx.bits_per_pixel,  &fdf->mlx.line_bytes, &fdf->mlx.endian);
-                                fdf->mlx.line_bytes /= 4;
-                                add_mlx_hook(fdf);
-                                draw_win(fdf);
-                                mlx_loop(fdf->mlx.mlx);
-                                return (1);
-                        }
-                }
-        }
-        return (0);
+	fdf->mlx.mlx = mlx_init();
+	if (fdf->mlx.mlx != NULL)
+	{
+		fdf->mlx.win = mlx_new_window(fdf->mlx.mlx, W_LENGHT, W_HEIGHT, "fdf");
+		if (fdf->mlx.win != NULL)
+		{
+			fdf->mlx.img = mlx_new_image(fdf->mlx.mlx, W_LENGHT - 50, W_HEIGHT - 50);
+			if (fdf->mlx.img != NULL)
+			{
+				fdf->mlx.buffer = (int *)mlx_get_data_addr(fdf->mlx.img,
+					&fdf->mlx.bits_per_pixel,  &fdf->mlx.line_bytes, &fdf->mlx.endian);
+				fdf->mlx.line_bytes /= 4;
+				add_mlx_hook(fdf);
+				draw_win(fdf);
+				mlx_loop(fdf->mlx.mlx);
+				return (1);
+			}
+		}
+	}
+	return (0);
 }
