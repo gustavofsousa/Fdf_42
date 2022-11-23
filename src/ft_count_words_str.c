d70bc5895c35f2f17d26810d4f671f762f8b062c
gusousa <gfsousa42@gmail.com>
2022-11-23 15:58:04 -0300

    New use of count_words()

--- src/ft_count_words_str.c
+++ src/ft_count_words_str.c
@@ -6,26 +6,31 @@
 /*   By: gusousa <gusousa@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 15:14:52 by gusousa           #+#    #+#             */
-/*   Updated: 2022/11/17 16:49:20 by gusousa          ###   ########.fr       */
+/*   Updated: 2022/11/23 15:57:02 by gusousa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_count_words_str(char const *s, char sep)
+int	count_words(char *str)
 {
 	int	i;
 	int	qtd_word;
-	int	next;
 
 	i = 0;
-	qtd_word = 0;
-	while (s[i])
+	qtd_word = 1;
+	while (str[i + 1])
 	{
-		next = i + 1;
-		if (s[i] != sep && (s[next] == sep || s[next] == '\0'))
+		if (str[i] != " " && str[i + 1] == " ")
 			qtd_word++;
 		i++;
 	}
 	return (qtd_word);
+}
+
+int	count_columns(t_fdf *fdf)
+{
+	int	n_col;
+
+	n_col = count_words(fdf->map.map_char[0])
 }
